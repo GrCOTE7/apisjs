40c9aea974513f177a4501d95c62f7c8e28020e0
Lionel COTE <grcote7@gmail.com>
2022-10-11 08:40:07 +0200

    Fix: Dynamic Backgroud

---
+++ wowjs/main.js
@@ -0,0 +1,6 @@
+import './style.scss'
+
+import './myAnimation.js'
+
+import anime from 'animejs/lib/anime.min.js';
+import { render } from 'sass';
---
+++ wowjs/myAnimation.js
@@ -0,0 +1,28 @@
+import anime from 'animejs/lib/anime.min.js';
+
+var updates = 0;
+
+let progressLogEl = document.getElementById('ball')
+let fillElt = document.getElementById('fill')
+
+// console.log(ball.innerHTML)
+
+anime({
+  targets: '#ball',
+  translateX: 260,
+  duration: 1000,
+  direction: 'alternate',
+  loop: 7,
+  easing: 'linear',
+  update: function (anim) {
+    updates++;
+    let prog = Math.round(anim.progress)
+    progressLogEl.innerHTML = prog + '%';
+    // console.log(typeOf(prog));
+    fillElt.style.width = prog + 9 + '%'
+    fillElt.style.backgroundColor = 'red'
+    fillElt.style.zIndex = 7;
+    if (prog < 1) fillElt.style.backgroundColor = 'rgba(255,0,0,0)'
+  }
+});
+console.log(progressLogEl)
