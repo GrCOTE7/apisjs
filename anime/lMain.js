48e501b468f901905f4584a8211c10e90ecbac84
Lionel COTE <grcote7@gmail.com>
2022-10-11 16:30:48 +0200

    Fix: EssaisAnimations

--- anime/lMain.js
+++ anime/lMain.js
@@ -1,8 +1,8 @@
-import anime from 'animejs/lib/anime.min.js';
+// import anime from 'animejs/lib/anime.min.js';
 // import { render } from 'sass';
 
 import './lStyle.scss'
-// import './linesAnimation.js'
+import './linesAnimation.js'
 
 
 // import './essais.scss'
--- anime/linesAnimation.js
+++ anime/linesAnimation.js
@@ -1,14 +1,16 @@
-import anime from './animejs/lib/anime.min.js'
+// import anime from './animejs/lib/anime.min.js'
 
-let updates=0;
+// let updates=0;
 
-anime({
-  targets: ".lines",
-  easing: 'easeOutQuad',
-  duration: 7000,
-  loop: 1,
-  update: function (a) {
-    updates++
+// anime({
+//   targets: ".line #blocLines",
+//   easing: 'easeOutQuad',
+//   duration: 7000,
+//   loop: 1,
+//   update: function (a) {
+//     updates++
 
-  }
-})
+//   }
+// })
+
+console.log('Ready.')
