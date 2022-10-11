5b4b9570bbec974585f90719a4a3ba911d59357e
Lionel COTE <grcote7@gmail.com>
2022-10-11 14:32:10 +0200

    Feat: SVG Text Effect

--- anime/myAnimation.js
+++ anime/myAnimation.js
@@ -1,14 +1,51 @@
-import anime from 'animejs/lib/anime.min.js';
+import anime from 'animejs/lib/anime.min.js'
 // import { defineConfig } from 'vite';
 
-var updates = 0;
+let updates = 0
+let updates2 = 0
 
 let progressLogEl = document.getElementById('ball')
 let fillElt = document.getElementById('fill')
 let ballElt = document.getElementById('ball')
 
-console.log(ballElt.style.backgroundColor)
+// console.log(ballElt.style.backgroundColor)
 // console.log(ball.innerHTML)
+
+let path = anime.path('#logo2-gc-svg path')
+let myLogo = document.getElementById('logo-gc-svg')
+
+console.log(myLogo)
+
+
+anime({
+  targets: '#logo-online-svg path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutQuad',
+  duration: 5000,
+  delay: 10,
+  direction: 'alternate',
+  loop: true
+})
+
+
+
+
+
+anime({
+  targets: "#emoji",
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'easeOutQuad',
+  delay: 1000,
+  duration: 7000,
+  direction: "alternate",
+  loop: true,
+  update: function (anim) {
+    updates++
+
+  }
+})
 
 anime({
   targets: '#ball',
@@ -16,16 +53,17 @@ anime({
   duration: 1000,
   direction: 'alternate',
   loop: 7,
-  easing: 'linear',
+  easing: 'easeOutCirc',
   update: function (anim) {
-    updates++;
+    updates2++;
     let prog = Math.round(anim.progress)
-    progressLogEl.innerHTML = prog + '%';
-    // console.log(typeOf(prog));
+    progressLogEl.innerHTML = prog + '%'
+    console.log(typeof prog)
+    myLogo.style.opacity = prog / 100
     fillElt.style.width = prog + 9 + '%'
-    fillElt.style.backgroundColor = 'red'
+    fillElt.style.backgroundColor = 'rgba(255,0,0,0.2)'
     fillElt.style.zIndex = 7;
     if (prog < 1) fillElt.style.backgroundColor = 'rgba(255,0,0,0)'
   }
 });
-console.log(progressLogEl)
+// console.log(progressLogEl)
