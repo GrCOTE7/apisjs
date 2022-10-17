db88705f23a1a2e4f3247432da755b0438cf0916
Lionel COTE <grcote7@gmail.com>
2022-10-17 13:30:00 +0200

    Feat: MindMap Quizz Project

--- quizz/main.js
+++ quizz/main.js
@@ -1,5 +1,9 @@
+
 import { getDataAsync as getAsync} from './scripts/api.js'
 import appendDom from './scripts/dom.js'
+import gc7 from './scripts/gc7.js';
+
+import './main.css';
 
 
-import './style.scss'
+
---
+++ quizz/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['**/*.html', './scripts/*gc7.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// content: ["./**/*.{html,js}"],
